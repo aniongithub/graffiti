8206d58b62aa35e754dccc81a43c4d6314c081b6
ananthonline <ananth@ananthonline.net>
2012-05-25 13:55:40 -0500

    Added license text to Samples.

--- Samples/Graffiti.Samples.Windows.MonoGame.Quake3Shader/Game.cs
+++ Samples/Graffiti.Samples.Windows.MonoGame.Quake3Shader/Game.cs
@@ -1,4 +1,19 @@
-﻿#region Using Statements
+﻿#region License and Copyright Notice
+// Copyright (c) 2010 Ananth Balasubramaniam
+// All rights reserved.
+// 
+// The contents of this file are made available under the terms of the
+// Eclipse Public License v1.0 (the "License") which accompanies this
+// distribution, and is available at the following URL:
+// http://www.opensource.org/licenses/eclipse-1.0.php
+// 
+// Software distributed under the License is distributed on an "AS IS" basis,
+// WITHOUT WARRANTY OF ANY KIND, either expressed or implied. See the License for
+// the specific language governing rights and limitations under the License.
+// 
+// By using this software in any fashion, you are agreeing to be bound by the
+// terms of the License.
+#endregion
 
 using Graffiti.Core.Brushes;
 using Graffiti.Core.Primitives;
@@ -7,8 +22,6 @@ using Graffiti.Math;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-
-#endregion
 
 namespace Graffiti.Samples.Windows.MonoGame.Quake3Shader
 {
