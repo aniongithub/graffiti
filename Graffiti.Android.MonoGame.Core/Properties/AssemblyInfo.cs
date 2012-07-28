8c2efa65663d42b0706e87ca4267bce040643ce2
ananthonline <ananth@ananthonline.net>
2012-07-28 10:50:25 -0500

    Adding a buildable Graffiti.Android.MonoGame.sln! :) Working samples coming up next and then Windows 8 support!

--- Graffiti.Android.MonoGame.Core/Properties/AssemblyInfo.cs
+++ Graffiti.Android.MonoGame.Core/Properties/AssemblyInfo.cs
@@ -18,19 +18,13 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Graffiti.Windows.MonoGame.Core")]
+[assembly: AssemblyTitle("Graffiti.Android.MonoGame.Core")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e339d25f-d125-4716-a77b-c886fd9dfdb7")]
+[assembly: Guid("09CD55FA-90FC-49CA-BE77-44F48C39824D")]
 
 [assembly: AssemblyFileVersion("1.0.0.0")]
